b8b39593276e927c5e9285fc83e2d59fc8370e10
Evelyn-Nobile <nobileevelyn1@gmail.com>
2024-08-21 19:22:44 -0300

    se modifica porcentaje de aprendizaje de dart

--- src/views/CurrentlyLearning.vue
+++ src/views/CurrentlyLearning.vue
@@ -4,10 +4,10 @@
 
         <div class="mt-12 flex flex-col items-start gap-1 barContainer">
             <h3 class="text-xl ">Flutter + Dart</h3>
-            <!-- <p class="text-justify  text-sm">198-hour course whose syllabus is: Review of HTML, CSS and JavaScript. SQL, MySQL, Scrum, Python, OOP and Flask.</p> -->
+        
 
 <div class="w-1/2 bg-gray-200 rounded-full dark:bg-gray-700 overflowBar ">
-  <div class="progress-bar text-xs font-medium text-purple-100 text-center p-0.5 leading-none rounded-full" style="width: 10%"> 10%</div>
+  <div class="progress-bar text-xs font-medium text-purple-100 text-center p-0.5 leading-none rounded-full" style="width: 20%"> 20%</div>
 </div>
         </div>
     
